cfbbf79e5d12aeebaf3f3b283ad64429aef2f4e9
johansen.keith@gmail.com <johansen.keith@gmail.com@2599881a-56e5-11de-bbaa-f1a44a2b9d47>
2009-06-12 04:04:05 +0000

    backup

--- trunk/data_preproc.m
+++ trunk/data_preproc.m
@@ -1,30 +1,77 @@
+clear
 load changedata.txt
 
 
-newData=zeros(1353,32);
+changedata(232,:)=[];
+changedata(1090,:)=[];
+changedata(1089,:)=[];
+
+num=400;
+
+newData=zeros(num,210);
 
 
-t=0
+t=25
 j=1;
 x=princomp(changedata);
-while t<313-24,
 
-x=princomp(changedata(:,t+1:t+24));
 
-y3=kMeans(changedata(:,t+1:t+24),3);
-y4=kMeans(changedata(:,t+1:t+24),4);
-y5=kMeans(changedata(:,t+1:t+24),5);
-y6=kMeans(changedata(:,t+1:t+24),6);
+pp=1;
+ss={};
+while t<(313-8),
 
-for i=1:1353,
-    newData(i,j)=changedata(i,t+1:t+24)*x(:,1);
-    newData(i,j+1)=changedata(i,t+1:t+24)*x(:,2);
+x=princomp(changedata(1:num,t-24:t));
+
+y3=kMeans(changedata(1:num,t-24:t),3);
+y4=kMeans(changedata(1:num,t-24:t),4);
+y5=kMeans(changedata(1:num,t-24:t),5);
+y6=kMeans(changedata(1:num,t-24:t),6);
+
+
+
+for i=1:num,
+    newData(i,j)=changedata(i,t-24:t)*x(:,1);
+
+if newData(i,j)>1,
+  ss{pp}=i;
+pp=pp+1;
+endif
+
+if newData(i,j)<-1,
+  ss{pp}=i;
+pp=pp+1;
+endif
+
+    newData(i,j+1)=changedata(i,t-24:t)*x(:,2);
+
+if newData(i,j+1)>1,
+  ss{pp}=i;
+pp=pp+1;
+endif
+
+if newData(i,j+1)<-1,
+  ss{pp}=i;
+pp=pp+1;
+endif
+
     newData(i,j+2)=y3(i);
     newData(i,j+3)=y4(i);
     newData(i,j+4)=y5(i);
     newData(i,j+5)=y6(i);
 endfor
 
-t=t+24;
+t=t+8;
 j=j+6;
 endwhile
+
+
+ss=unique(cell2mat(ss));
+
+
+
+for i=1:size(ss)(2),
+	newData(ss(i)-(i-1),:)=[];
+endfor
+
+
+save -ascii newdata.csv newData
