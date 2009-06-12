01a4df084a7e30fc828db21b08359a5c8767b334
johansen.keith <johansen.keith@gmail.com>
2009-06-12 14:02:56 +0000

    multiple windows

--- data_preproc.m
+++ data_preproc.m
@@ -2,10 +2,6 @@ clear
 load changedata.txt
 
 
-changedata(232,:)=[];
-changedata(1090,:)=[];
-changedata(1089,:)=[];
-
 num=400;
 
 newData=zeros(num,210);
@@ -22,10 +18,10 @@ while t<(313-8),
 
 x=princomp(changedata(1:num,t-24:t));
 
-y3=kMeans(changedata(1:num,t-24:t),3);
-y4=kMeans(changedata(1:num,t-24:t),4);
 y5=kMeans(changedata(1:num,t-24:t),5);
 y6=kMeans(changedata(1:num,t-24:t),6);
+y7=kMeans(changedata(1:num,t-24:t),7);
+y8=kMeans(changedata(1:num,t-24:t),8);
 
 
 
@@ -54,15 +50,22 @@ if newData(i,j+1)<-1,
 pp=pp+1;
 endif
 
-    newData(i,j+2)=y3(i);
-    newData(i,j+3)=y4(i);
-    newData(i,j+4)=y5(i);
-    newData(i,j+5)=y6(i);
+    newData(i,j+2)=y5(i);
+    newData(i,j+3)=y6(i);
+    newData(i,j+4)=y7(i);
+    newData(i,j+5)=y8(i);
 endfor
 
 t=t+8;
 j=j+6;
 endwhile
+
+
+
+
+save -ascii newdata.csv newData
+
+
 
 
 ss=unique(cell2mat(ss));
@@ -72,6 +75,3 @@ ss=unique(cell2mat(ss));
 for i=1:size(ss)(2),
 	newData(ss(i)-(i-1),:)=[];
 endfor
-
-
-save -ascii newdata.csv newData
